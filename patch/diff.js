882da832fa019cf32604b4736298d3dd1a5a1df7
ivan-shork <494476749@qq.com>
2023-09-02 12:16:40 +0800

    双端diff

--- patch/diff.js
+++ patch/diff.js
@@ -1,6 +1,6 @@
 import { patch } from "./index.js"
 import { unmount } from "./unmount.js"
-import { setElementText } from "./utils.js"
+import { insert, setElementText } from "./utils.js"
 
  /**
      * 
@@ -31,55 +31,11 @@ import { setElementText } from "./utils.js"
 
 
             // 方式一 见readme 遍历其中children最短的，然后一一对比，然后在找出超出的进行遍历，如果老节点长度大于新节点，则有卸载，反之是挂载
-            // 方式二 利用key来复用vnode，并且调整好顺序
+            // 方式二 利用key来复用dom，并且调整好dom的顺序
+            // 方式三 双端diff
 
-            let lastIndex = 0 
-            for(let i = 0;i<n2Child.length; i++) {
-                const newVnode = n2Child[i]
-                let find = false
-                for(let j = 0;j < n1Child.length; j++) {
-                    const oldVnode = n1Child[j]
-                    if(n2Child[i].key === n1Child[j].key) {
-                        find = true
-                        patch(oldVnode, newVnode, el)
-                        if(j < lastIndex) {
-                            // 说明vnode对应的真实dom需要移动
-                            const prevVnode = n2Child[i - 1]
-                            if(prevVnode) {
-                                // 找到前一个新的vnode的下一个节点作为猫点，插到它前面去
-                                const anchor = prevVnode.el.nextSibling
-                                insert(newVnode.el, el, anchor)
-                            }
-                        } else {
-                            lastIndex = j
-                        }
-                        break
-                    }
-                }
-
-                // 代表没有找到可以复用的key的新节点，需要进行挂载
-                if(!find) {
-                    const prevVnode = n2Child[i - 1]
-                    let anchor = null
-                    if(prevVnode) {
-                        // 如果有前一个，则挂在前一个后面
-                        anchor = prevVnode.el.nextSibling
-                    } else {
-                        // 证明是第一个
-                        anchor = el.firstChild
-                    }
-                    patch(null, newVnode, el, anchor)
-                }
-            }
-
-            // 当更新结束时，我们还需要判断是否有遗留的旧节点需要删除
-            for(let i = 0; i<n1Child.length; i++) {
-                const oldNode = n1Child[i]
-                const has = n2Child.find(node => node.key === oldNode.key)
-                if(!has) {
-                    unmount(oldNode)
-                }
-            }
+            doubleDiff(n1, n2)
+            
         } else {
             // 如果没有子节点，或子节点是文本节点的情况
             // 只需要清空文本，并且一个个patch 挂载新的子节点就行了
@@ -92,6 +48,77 @@ import { setElementText } from "./utils.js"
             setElementText(el, '')
         } else if(Array.isArray(n1Child)) {
             n1Child.forEach(child => unmount(child))
+        }
+    }
+}
+
+// 双端diff
+function doubleDiff (n1, n2, container) {
+    let oldStartIndex = 0
+    let oldEndIndex = n1.length - 1
+    let newStartIndex = 0
+    let newEndIndex = n2.length - 1
+    while(oldStartIndex <= oldEndIndex && newStartIndex <= newEndIndex) {
+        let oldStartNode = n1[oldStartIndex]
+        let oldEndNode = n1[oldEndIndex]
+        let newStartNode = n2[newStartIndex]
+        let newEndNode = n2[newEndIndex]
+        if(!oldStartNode) {
+            // 在刚才被设为undefined的那个，证明已经处理过了,跳过即可
+            ++oldStartIndex
+        }else if(!oldEndNode) {
+            --oldEndIndex
+        }else if(oldStartIndex.key === newStartNode.key) {
+            // 新头旧头
+            patch(oldStartNode, newStartNode, container)
+            ++oldStartIndex
+            ++newStartIndex
+        } else if(oldEndNode.key === newEndNode.key) {
+            // 旧尾新尾
+            patch(oldEndNode, newEndNode, container)
+            --oldEndIndex
+            --newEndIndex
+        } else if(oldStartNode.key === newEndNode.key) {
+            // 旧头新尾
+            patch(oldStartNode, newEndNode, container)
+            insert(oldStartNode.el, container, oldEndNode.el.nextSibling)
+            ++oldStartIndex
+            --newEndIndex
+        } else if(newStartNode.key === oldEndNode.key){
+            // 新头旧尾
+            patch(oldEndNode, newStartNode, container)
+            insert(oldEndNode.el, container, oldStartNode.el)
+            --oldEndIndex
+            ++newStartIndex
+        } else {
+            // 假设都找不到，那我就需要用新头对比旧的一组子节点去找
+            let find = false
+            for(let i = 0; i < n1.length; i++) {
+                if(newStartNode.key === n1[i].key) {
+                    find = true
+                    patch(n1[i], newStartNode, container)
+                    insert(n1[i].el, container, oldStartNode.el)
+                    n1[i] = undefined
+                    break;
+                }
+            }
+            if(!find) {
+                // 没有找到， 证明这个是新增的, 去挂载
+                patch(null, newStartNode, container, oldStartNode.el)
+            }
+            ++newStartIndex
+        }
+    }
+
+    if(oldEndIndex < oldStartIndex && newStartIndex <= newEndIndex) {
+        // 对比完，最后新节点还剩 证明还有东西需要挂载
+        for(let i = newStartIndex; i <= newEndIndex; i++) {
+            patch(null, n2[i], container, oldStartNode.el)
+        }
+    } else if(newEndIndex < newStartIndex && oldStartIndex <= oldEndIndex) {
+        // 对比完，最后旧节点还剩 证明还有东西需要挂载
+        for(let i = oldStartIndex; i <= oldEndIndex; i++) {
+            unmount(n1[i])
         }
     }
 }
